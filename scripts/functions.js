35ecaa73008daf44aeea1f902dcaa76d1a347fe8
federicoacker <acker.federico@gmail.com>
2026-04-03 17:36:19 +0200

    Spostato le funzioni tutte nel file functions.js

--- scripts/functions.js
+++ scripts/functions.js
@@ -78,3 +78,57 @@ function createTicket(name, surname, ageCategory, price) {// La funzione che cre
     ticketCosto.innerText = `${price.toFixed(2)} ${'\u20AC'} `;
 
 }
+
+// FUNZIONE HANDLER PER IL SUBMIT
+function formSubmitHandler(event){
+    event.preventDefault();
+
+    // LOAD DELLE VARIABILI //
+
+    let name = nameElem.value;
+    let surname = surnameElem.value; 
+    let kilometers = Number(kilomElem.value);
+    const ageCategory = ageElem.value; // Per questi, non ho bisogno di fare validazione, visto che è un select e quindi i value glieli ho dati io
+
+    // VALIDAZIONE //
+    const stringValidator = validatorGenerator("string"); // Creo la funzione di validazione delle stringhe
+    const numberValidator = validatorGenerator("number"); // Creo la funzione di validazione dei numeri
+
+    name = stringValidator(name);
+    if(name === -1){
+        alert("Hai inserito un valore non valido nel Nome");
+        return;
+    }
+    surname = stringValidator(surname);
+    if(surname === -1){
+        alert("Hai inserito un valore non valido nel Cognome");
+        return;
+    }
+    kilometers = numberValidator(kilometers);
+    if(kilometers === -1){
+        alert("Hai inserito un valore non valido per i Chilometri");
+        return;
+    }
+
+    // POST VALIDAZIONE //
+    // Per questo, devo prima andare a creare l'html del biglietto completato
+    const price = calculatePrice(kilometers, ageCategory);
+
+    createTicket(name, surname, ageCategory, price);
+    nameElem.value = "";
+    surnameElem.value = "";
+    kilomElem.value = "";
+    ageElem.value = "0";
+
+    ticketWrapper.classList.remove("d-none");
+    newTicketBtn.classList.remove("d-none");
+    formWrapper.classList.add("d-none");
+}
+
+// FUNZIONE HANDLER PER IL BOTTONE PER RITORNARE ALLA SCHERMATA INIZIALE
+
+function newTicketBtnClickHandler(){
+    ticketWrapper.classList.add("d-none");
+    newTicketBtn.classList.add("d-none");
+    formWrapper.classList.remove("d-none");
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-//Prima cosa, mi serve recuperare i vari field da cui vorrò leggere i dati
+//RECUPERO DEGLI ELEMENTI HTML
 const formWrapper = document.querySelector("#form-wrapper");
 const ticketWrapper = document.querySelector("#ticket-wrapper");
 const formElem = document.querySelector("#ticket-form")
@@ -8,60 +8,6 @@ const kilomElem = document.querySelector("#user-form-kilometers");
 const ageElem = document.querySelector("#user-form-age-category");
 const newTicketBtn = document.querySelector("#new-ticket-btn");
 
-
-
-//Adesso, andiamo a creare la funzione che si occuperà di recuperare i dati quando viene fatto il submit
-function formSubmitHandler(event){
-    event.preventDefault();
-
-    // LOAD DELLE VARIABILI //
-
-    let name = nameElem.value;
-    let surname = surnameElem.value; 
-    let kilometers = Number(kilomElem.value);
-    const ageCategory = ageElem.value; // Per questi, non ho bisogno di fare validazione, visto che è un select e quindi i value glieli ho dati io
-
-    // VALIDAZIONE //
-    const stringValidator = validatorGenerator("string"); // Creo la funzione di validazione delle stringhe
-    const numberValidator = validatorGenerator("number"); // Creo la funzione di validazione dei numeri
-
-    name = stringValidator(name);
-    if(name === -1){
-        alert("Hai inserito un valore non valido nel Nome");
-        return;
-    }
-    surname = stringValidator(surname);
-    if(surname === -1){
-        alert("Hai inserito un valore non valido nel Cognome");
-        return;
-    }
-    kilometers = numberValidator(kilometers);
-    if(kilometers === -1){
-        alert("Hai inserito un valore non valido per i Chilometri");
-        return;
-    }
-
-    // POST VALIDAZIONE //
-    // Per questo, devo prima andare a creare l'html del biglietto completato
-    const price = calculatePrice(kilometers, ageCategory);
-
-    createTicket(name, surname, ageCategory, price);
-    nameElem.value = "";
-    surnameElem.value = "";
-    kilomElem.value = "";
-    ageElem.value = "0";
-
-    ticketWrapper.classList.remove("d-none");
-    newTicketBtn.classList.remove("d-none");
-    formWrapper.classList.add("d-none");
-}
-
-function newTicketBtnClickHandler(){
-    ticketWrapper.classList.add("d-none");
-    newTicketBtn.classList.add("d-none");
-    formWrapper.classList.remove("d-none");
-}
-
+// AGGIUNGO EVENT LISTENER
 formElem.addEventListener("submit", formSubmitHandler);
-
 newTicketBtn.addEventListener("click", newTicketBtnClickHandler);
